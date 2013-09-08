f0fc3531565a26f6b42d99e7dbccd7dde774e709
Chase Wilson <chase@agroism.com>
2013-09-08 14:34:59 -0700

    Added tests.

--- test/layout.js
+++ test/layout.js
@@ -7,58 +7,58 @@ var assert = require('assert')
 
 describe('Layout', function () {
 
-	var layout
-	beforeEach(function () {
-		layout = new Layout
-	})
+  var layout
+  beforeEach(function () {
+    layout = new Layout
+  })
 
-	describe('Layout#constructor', function () {
-		
-		it ('should return a `Layout` instance', function () {
-			assert.ok(layout instanceof Layout)
-		})
+  describe('Layout#constructor', function () {
+  
+    it ('should return a `Layout` instance', function () {
+      assert.ok(layout instanceof Layout)
+    })
 
-	})
+  })
 
-	describe('Layout#create', function () {
-		var Constructor = Layout.create()
-		it('should return a function that constructs into a Layout instance', function () {
-			assert.ok(new Constructor instanceof Layout)
-		})
-	})
+  describe('Layout#create', function () {
+    var Constructor = Layout.create()
+    it('should return a function that constructs into a Layout instance', function () {
+      assert.ok(new Constructor instanceof Layout)
+    })
+  })
 
-	describe('#addChild', function () {
-		
-		it('should add a child block to a referenced parent', function () {
-			var zeeBlock = new block({name: 'some.child'})
-			layout.addChild('root', zeeBlock)
-			assert.equal(layout.reference('some.child'), zeeBlock)
-		})
+  describe('#addChild', function () {
+  
+    it('should add a child block to a referenced parent', function () {
+      var zeeBlock = new block({name: 'some.child'})
+      layout.addChild('root', zeeBlock)
+      assert.equal(layout.reference('some.child'), zeeBlock)
+    })
 
-		it('should add a block to the root if no reference is passed', function () {
-			var blockYo = new block({name:"blurg"})
-			layout.addChild(blockYo)
-			assert.equal(layout.root.children.length, 1)
-		})
+    it('should add a block to the root if no reference is passed', function () {
+      var blockYo = new block({name:"blurg"})
+      layout.addChild(blockYo)
+      assert.equal(layout.root.children.length, 1)
+    })
 
-	})
+  })
 
-	describe('#removeChild', function () {
+  describe('#removeChild', function () {
 
-		it('', function () {
+    it('', function () {
 
-		})
+    })
 
-	})
+  })
 
-	describe('#reference', function () {
+  describe('#reference', function () {
 
-			it('should return a block using a the blocks `name` property for a reference', function () {
-				var thing = new block({name: 'thing'})
-				layout.addChild('root', thing)
-				assert.equal(layout.reference('thing'), thing)
-			})
+    it('should return a block using a the blocks `name` property for a reference', function () {
+      var thing = new block({name: 'thing'})
+      layout.addChild('root', thing)
+      assert.equal(layout.reference('thing'), thing)
+    })
 
-	})
+  })
 
 })
